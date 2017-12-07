ee006b8f76ef5894240aa1720c1135b0f16d24b3
Kees de Jong <keesdejong+bugs@gmail.com>
2017-12-07 13:49:41 +0100

    Simplify the write dd

--- bin/srun.sh
+++ bin/srun.sh
@@ -4,7 +4,7 @@ size=10 # In GiB
 
 if [[ $1 == "write" ]]; then
 	lfs setstripe -S 2M -c -1 $(hostname).bin
-	time dd if=/dev/urandom of=$(hostname).bin bs=1024M count=$size iflag=fullblock conv=fdatasync status=progress 2>> dd.log
+	time dd if=/dev/zero of=$(hostname).bin bs=1024M count=$size status=progress 2>> dd.log
 	echo "$?: $size GiB blob done on $(hostname)" >> dd.log
 elif [[ $1 == "read" ]]; then
 	time dd if=/srv/os3/blub.dd of=/dev/null status=progress 2>> dd.log
