e78422a1dc2f8ae02a8f9235cfe490944b915276
Kees de Jong <keesdejong+bugs@gmail.com>
2017-12-29 09:21:33 -0500

    Added comments to describe the batch scripts

--- bin/sbatch.sh
+++ bin/sbatch.sh
@@ -4,11 +4,9 @@
 #SBATCH -N 1
 resourcedir="/srv/os3/job"
 jobdir="/srv/os3/job-${SLURM_JOB_ID}"
-if mkdir "$jobdir"
-then
-  if cd "$jobdir"
-  then
-    srun "$resourcedir/srun.sh"		# Give either [read|write] as parameter
+if mkdir "$jobdir"; then
+  if cd "$jobdir"; then
+    srun "$resourcedir/srun.sh"	$1	# Give either [read|write] as parameter
   else
     echo "Could not cd into the job directory"
     exit 1
--- bin/srun.sh
+++ bin/srun.sh
@@ -3,10 +3,16 @@
 size=10 # In GiB
 
 if [[ $1 == "write" ]]; then
+	# Set stripe count to max for the bin file
 	lfs setstripe -S 2M -c -1 $(hostname).bin
+	# Write the bin file striped over all OSTs
 	time dd if=/dev/zero of=$(hostname).bin bs=1024M count=$size status=progress 2>> dd.log
 	echo "$?: $size GiB blob done on $(hostname)" >> dd.log
 elif [[ $1 == "read" ]]; then
+	# Read a generic dd file which as been striped over all OSTs
 	time dd if=/srv/os3/blub.dd of=/dev/null status=progress 2>> dd.log
 	echo "$?: $size GiB blob done on $(hostname)" >> dd.log
+else
+	echo "Provide argument [read|write]"
+	exit 1
 fi
